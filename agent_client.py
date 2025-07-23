03c801ed566420684ca97809a46834485539a097
Santhegudda <VS4843@zebra.com>
2025-07-23 22:46:31 +0530

    first commit

--- agent_client.py
+++ agent_client.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def run_client():
-    uri = "ws://13.58.212.239:8000/ws/commands"  # Match FastAPI route
+    uri = "ws://13.58.212.239:8000/ws"  # Match FastAPI route
     async with websockets.connect(uri) as websocket:
         await websocket.send("deploy")
         response = await websocket.recv()
