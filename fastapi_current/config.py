b43d73ca573f7760997828d02610c2c895d67cb2
Santhegudda <VS4843@zebra.com>
2025-07-26 16:17:50 +0530

    dot env and requiremnts.txt updated

--- fastapi_current/config.py
+++ fastapi_current/config.py
@@ -8,3 +8,12 @@ MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_DB = os.getenv("MYSQL_DB")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+# SQLAlchemy engine and session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
