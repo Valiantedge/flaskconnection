28399a5536b83741f7f0f7edfee7bd2bc318d201
Santhegudda <VS4843@zebra.com>
2025-07-26 15:52:38 +0530

    Inital commit

--- fastapi_current/app.py
+++ fastapi_current/app.py
@@ -3,6 +3,9 @@ from routes import auth, agent, command
 from websocket import server as ws_server
 from ansible_api import long_command
 
+from models import Base
+from config import engine
+
 app = FastAPI()
 
 # Routers
@@ -11,3 +14,6 @@ app.include_router(agent.router, prefix="/api/agent")
 app.include_router(command.router, prefix="/api/command")
 app.include_router(ws_server.router)
 app.include_router(long_command.router, prefix="/api/ansible")
+
+# Automatically create tables if they do not exist
+Base.metadata.create_all(engine)
