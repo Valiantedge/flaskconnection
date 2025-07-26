1a3afee11c87dffa2123a81f218766df3402164d
Santhegudda <VS4843@zebra.com>
2025-07-26 16:09:39 +0530

    Inital commit

--- fastapi_current/config.py
+++ fastapi_current/config.py
@@ -1,5 +1,10 @@
-MYSQL_USER = "appuser"
-MYSQL_PASSWORD = "Valiantops$2025"
-MYSQL_HOST = "localhost"
-MYSQL_DB = "noops"
-JWT_SECRET_KEY = "supersecret"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DB = os.getenv("MYSQL_DB")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
