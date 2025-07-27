bc1cd287db6083a7f58775fee589bf93709c765f
Santhegudda <VS4843@zebra.com>
2025-07-27 19:43:41 +0530

    old cleniup

--- fastapi_current/app.py
+++ fastapi_current/app.py
@@ -6,14 +6,15 @@ from ansible_api import long_command
 from models import Base
 from config import engine
 
+
 app = FastAPI()
 
-# Routers
-app.include_router(auth.router, prefix="/api/user")
+# Routers (ordered for logical SaaS onboarding flow)
 app.include_router(agent.router, prefix="/api/agent")
+app.include_router(auth.router, prefix="/api/user")
 app.include_router(command.router, prefix="/api/command")
-app.include_router(ws_server.router)
 app.include_router(long_command.router, prefix="/api/ansible")
+app.include_router(ws_server.router)
 
 # Automatically create tables if they do not exist
 Base.metadata.create_all(engine)
